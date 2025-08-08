398906d377a28cbf4ed5f3004a173b71b54ad217
GOPINATH7R <gopi311201@gmail.com>
2025-08-08 11:34:27 +0530

    Initial commit

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { Pool } = require('pg');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
 
 const app = express();
 const port = 3000;
